b31e60875969e6fb4f0313b23dd377ba011663be
ballet <c0a25152f3@edu.teu.ac.jp>
2026-06-23 14:54:22 +0900

    爆弾とビームの衝突判定

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -95,7 +95,7 @@ class Beam:
         """
         self.img = pg.image.load(f"fig/beam.png")
         self.rct = self.img.get_rect()
-        self.rct.centry = bird.rct.centry
+        self.rct.centery = bird.rct.centery
         self.rct.left = bird.rct.right
         self.vx, self.vy = +5, 0
 
@@ -158,17 +158,26 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
             # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        
+        if bomb is not None:
+            if beam is not None:
+                if beam.rct.colliderect(bomb.rct):
+                    beam = None
+                    bomb = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        beam.update(screen)   
-        bomb.update(screen)
+        if beam is not None:
+            beam.update(screen) 
+        if bomb is not None:      
+            bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
